f4d70abb52551b475ab4986826a8b8c54d9f6182
Adrian Paparelli <ajpaparelli@gmail.com>
2016-01-14 16:54:38 -0500

    added comments, and moved struct to stringtable.h

--- csci5220/src/stringtable.h
+++ csci5220/src/stringtable.h
@@ -1,7 +1,29 @@
+/*
+
+	Name: Adrian J. Paparelli
+	Class: CSCI 5220
+	Session: Spring 2016
+
+	Description: stringtable.h defines all functions used in storing 
+		strings into a string list.
+
+	Change Log:
+	2016-11-14: Initial Revision
+
+*/
+
 #ifndef STRINGTABLE_H
 #define STRINGTABLE_H
 
 #include <stdlib.h>
+
+typedef struct node
+{
+	char* data;
+	struct node* next;
+} Node;
+
+typedef Node* List;
 
 char* perm(const char* s);
 
