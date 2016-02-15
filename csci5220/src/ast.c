8ca25e9040e72ef2117b4604ac3dab1a138fca32
Adrian Paparelli <ajpaparelli@gmail.com>
2016-02-14 22:12:18 -0500

    new

--- csci5220/src/ast.c
+++ csci5220/src/ast.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #include "ast.h"
 #include "allocate.h"
 
 AST numberNode(int n)
 {
-	AST t = NEW(AST);
+	AST t = NEW(ASTNODE);
 	t->kind = NUMBER_NK;
 	t->fields.intval = n;
 	return t;
@@ -11,7 +12,7 @@ AST numberNode(int n)
 
 AST idNode(const char* s)
 {
-	AST t = NEW(AST);
+	AST t = NEW(ASTNODE);
 	t->kind = ID_NK;
 	t->fields.stringval = s;
 
@@ -20,7 +21,7 @@ AST idNode(const char* s)
 
 AST applyNode(AST A, AST B)
 {
-	AST t = NEW(AST);
+	AST t = NEW(ASTNODE);
 	t->kind = APPLY_NK;
 	t->fields.subtrees.s1 = A;
 	t->fields.subtrees.s2 = B;	
@@ -28,11 +29,17 @@ AST applyNode(AST A, AST B)
 }
 
 
-AST applyOp(AST A, AST B, int OpKind)
+AST applyOp(AST A, AST B, char opKind)
 {
-	AST t = NEW(AST);
-	t->kind = BASICOP_NK;
-	t->extra = OpKind;
+	AST t = NEW(ASTNODE);
+	int op;
+	t->kind = OP_NK;
+	
+	if(opKind == '+') 
+		op = PLUSOP_OK;
+	else if(opKind == '*')
+		op = MULTOP_OK;
+	t->extra = op;	
 	t->fields.subtrees.s1 = A;
 	t->fields.subtrees.s2 = B;
 	return t;
@@ -40,35 +47,62 @@ AST applyOp(AST A, AST B, int OpKind)
 
 AST applyCOLON(AST A, AST B)
 {
-	AST t = NEW(AST);
+	AST t = NEW(ASTNODE);
 	t->kind = COLON_NK;
 	t->fields.subtrees.s1 = A;
-	//if(blah)
-		t->fields.subtrees.s2 = B;
-	//else
-	//t->fields.subtrees.s2 = EMPTYLIST
+	t->fields.subtrees.s2 = B;
 	return t;
 }
 
 AST applyCONS(AST A, AST B)
 {
-	AST t = NEW(AST);
+	AST t = NEW(ASTNODE);
 	t->kind = CONS_NK;
 	t->fields.subtrees.s1 = A;
-	//if(blah)
-		t->fields.subtrees.s2 = B;
-	//else
-	//t->fields.subtrees.s2 = EMPTYLIST
+	t->fields.subtrees.s2 = B;
+	return t;
+}
+
+AST applyCond(AST A, AST B, AST C, int CondKind)
+{
+	AST t = NEW(ASTNODE);
+	t->kind = IFOP_NK;
+	t->extra = CondKind;
+	t->fields.subtrees.s1 = A;
+	t->fields.subtrees.s2 = B;
+	t->fields.subtrees.s3 = C;
 	return t;
 }
 
 AST applyFunc(AST A, int FuncKind)
 {
-	AST t = NEW(AST);
-	t->kind = BASIC_FUNK_NK;
+	AST t = NEW(ASTNODE);
+	t->kind = BASIC_FUNC_NK;
 	t->extra = FuncKind;
 	t->fields.subtrees.s1 = A;
 	return t;
 }
 
+void displayAST(AST A)
+{
+	if(A->kind == NUMBER_NK)
+		printf("  %d\n",A->fields.intval);
+	else if(A->kind == ID_NK)
+		printf("  %s\n",A->fields.stringval);
+	else
+ 	{
 
+	if(A->extra == PLUSOP_OK)
+		printf("+\n");
+	else if(A->extra == MULTOP_OK)
+		printf("*\n");
+
+	if(A->fields.subtrees.s1 != '\0')
+		displayAST(A->fields.subtrees.s1);
+	if(A->fields.subtrees.s2 != '\0')
+		displayAST(A->fields.subtrees.s2);
+	if(A->fields.subtrees.s3 != '\0')
+		displayAST(A->fields.subtrees.s3);
+	}
+}
+	
--- csci5220/src/ast.h
+++ csci5220/src/ast.h
@@ -1,6 +1,49 @@
 #ifndef AST_H_
 #define AST_H_
 
+
+
+#define NUMBER_NK	1
+#define APPLY_NK 	2
+#define ID_NK		3
+#define OP_NK		4
+
+#define PLUSOP_OK       40
+#define SUBOP_OK	41
+#define MULTOP_OK	42
+#define DIVOP_OK	43
+#define GT_OK		44
+#define LT_OK		45
+#define EQ_OK		46
+#define ANDOP_OK	47
+#define OROP_OK		48
+#define NOTOP_OK	49
+#define SEMICOL_OK	50
+
+#define IFOP_NK		5
+#define IF_OK		51
+#define ELSE_OK		52
+
+#define BASIC_FUNC_NK	6
+#define ISNULL_FK	60
+#define ISFUNC_FK	61
+#define ISINT_FK	62
+#define ISBOOL_FK	63
+#define ISACTION_FK	64
+#define ISCHAR_FK	65
+#define HEAD_FK		66
+#define TAIL_FK		67
+#define PRINT_FK	68
+#define PRILST_FK	69
+#define PROD_FK		70
+#define READC_FK	71
+#define READI_FK	72
+
+
+
+#define COLON_NK 	7
+#define CONS_NK		8
+
 typedef struct astnode {
 	int kind;
 	int mark;
@@ -10,43 +53,20 @@ typedef struct astnode {
 		int intval;
 		const char* stringval;
 		struct {
-			struct ast* s1;
-			struct ast* s2;
-			struct ast* s3;
+			struct astnode* s1;
+			struct astnode* s2;
+			struct astnode* s3;
 		} subtrees;
 	} fields;
 }
 ASTNODE;
 
 typedef ASTNODE* AST;
+int charpad;
 
-#define NUMBER_NK	1
-#define APPLY_NK 	2
-#define ID_NK		3
+AST numberNode(int n);
 
-#define ADDOP_NK	4
-#define PLUSOP_OK       41
-#define SUBOP_OK	42
+AST applyOp(AST A, AST B, char opKind);
 
-#define MULOP_NK	5
-#define MULTOP_OK	51
-#define DIVOP_OK	52
-
-#define RELOP_NK	6
-#define GT_OK		61
-#define LT_OK		62
-#define EQ_OK		63
-
-#define BASIC_FUNC_NK	7
-#define ISNULL_FK	71
-#define ISFUNC_FK	72
-#define ISINT_FK	73
-#define ISBOOL_FK	74
-#define ISACTION_FK	75
-#define ISCHAR_FK	76
-
-#define BASIC_OP_NK      77
-#define COLON_NK 8
-#define CONS_NK	9
-
+void displayAST();
 #endif
