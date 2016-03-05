0e96d53b8b781133ffd26708ec43ed6dbf4a87e9
Adrian Paparelli <ajpaparelli@gmail.com>
2016-03-04 21:36:05 -0500

    initial revision

---
+++ csci5220/src/simplify.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ast.h"
+#include "simplify.h"
+#include "symboltable.h"
+
+AST simplify(AST t)
+{
+	if(t != NULL)
+	{
+		if(t->kind == ID_NK)
+			return getTree(t->subtrees.stringval);
+		else if((t->kind == NUMBER_NK) || (t->kind == CONSTCHAR_NK)
+			 || (t->kind == EMPTYLIST) || (t->kind == BOOL_NK))  
+			return t; 
+		else if(t->kind == CONS_NK)
+			return t;
+		else if(t->kind == COLON_NK)
+		{
+			return applyCONS(simplify(t->subtrees.s1), simplify(t->subtrees.s2));
+		}
+		else if(t->kind == OP_NK)
+		{
+			AST s = simplify(t->subtrees.s1);
+			AST r = simplify(t->subtrees.s2);
+			if(t->extra = PLUSOP_OK)
+			{
+				if((s->kind == NUMBER_NK) && (r->kind == NUMBER_NK))
+				{
+					int x = s->subtrees.intval + r->subtrees.intval;
+					return numberNODE(x);
+				}
+			}
+			else if(t->extra = SUBOP_OK)
+			{
+				if((s->kind == NUMBER_NK) && (r->kind == NUMBER_NK))
+				{
+					int x = s->subtrees.intval - r->subtrees.intval;
+					return numberNODE(x);
+				}
+			}
+			else if(t->extra = MULTOP_OK)
+			{
+				if((s->kind == NUMBER_NK) && (r->kind == NUMBER_NK))
+				{
+					int x = s->subtrees.intval * r->subtrees.intval;
+					return numberNODE(x);
+				}
+			}
+			else if(t->extra = DIVOP_OK)
+			{
+				if((s->kind == NUMBER_NK) && (r->kind == NUMBER_NK))
+				{
+					int x = s->subtrees.intval / r->subtrees.intval;
+					return numberNODE(x);
+				}
+			}
+		}
+
+	}
+	return NULL;
+
+}
---
+++ csci5220/src/simplify.h
@@ -0,0 +1,8 @@
+#ifndef SIMPLIFY_H_
+#define SIMPLIFY_H_
+
+#include "ast.h"
+
+AST simplify(AST t);
+
+#endif SIMPLIFY_H_
