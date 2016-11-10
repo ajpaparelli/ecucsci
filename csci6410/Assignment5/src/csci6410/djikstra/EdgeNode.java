6946604f03241a1a466723f89bb302683d9cf30e
Adrian Paparelli <ajpaparelli@users.noreply.github.com>
2016-11-09 21:48:36 -0500

    Add Comments

--- csci6410/Assignment5/src/csci6410/djikstra/EdgeNode.java
+++ csci6410/Assignment5/src/csci6410/djikstra/EdgeNode.java
@@ -1,24 +1,40 @@
+/*
+	Name: Adrian Paparelli
+	
+	Class: CSCI 6410
+	
+	Assignment 5
+
+Class Name: EdgeNode.java
+		
+	Class Definition:  Defines the container class for the edges in the Graphs adjacency list.
+	
+*/
 
 public class EdgeNode {
 	private Edge edge;
 	private EdgeNode next;
 	
+	//Initalize the edge node to "edge"
 	public EdgeNode(Edge edge)
 	{
 		this.edge = edge;
 		this.next = null;
 	}
 	
+	//Set the value of the next pointer
 	public void setNext(EdgeNode node)
 	{
 		this.next = node;
 	}
 	
+	//Get the value of the next pointer
 	public EdgeNode getNext()
 	{
 		return this.next;
 	}
 	
+	//Get the edge object contained in the edgenode
 	public Edge getEdge()
 	{
 		return this.edge;
